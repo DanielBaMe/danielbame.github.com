7cca4a23eca40aae30bae15b5dcf89bab2271c83
Daniel <1fbaesmeneses@gmail.com>
2019-06-05 16:42:12 -0500

    Agregamos el proyecto boletines

---
+++ cv/boletines/javascript/main.js
@@ -0,0 +1,28 @@
+let cerrado = document.querySelectorAll(".close");
+
+cerrado.forEach(function(cl){
+  cl.addEventListener("click",function(ev){
+    ev.preventDefault();
+    let content = document.querySelector(".content");
+    //quitar animaciones
+    content.classList.remove("zoomInUp");
+    content.classList.remove("animated");
+
+    //poner animaciones
+    content.classList.add("bounceOutRight");
+    content.classList.add("animated");
+
+    setTimeout(function(){
+      location.href = "/";
+    },600);
+
+  });
+});
+
+let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+  td.addEventListener('click',function(){
+    console.log("click");
+  })
+})
